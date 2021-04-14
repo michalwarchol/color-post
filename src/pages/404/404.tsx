74dac3489ae5beb63e9a778a87c8d2e9dc2d5ffe
Michał Warchoł <imsimilar@gmail.com>
2021-04-14 14:40:12 +0200

    :question: Error page 404

---
+++ src/pages/404/404.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+import Topbar from "../../components/Topbar/Topbar"
+import Footer from "../../components/Footer/Footer"
+import Button from "../../components/Button/Button"
+
+const NotFound = () => {
+
+    const goHome = () => {
+        window.location.href="/";
+    }
+
+    return (
+        <div className="notFound  d-flex flex-column">
+            <Topbar />
+            <div className="d-flex container flex-column flex-grow-1 justify-content-center align-items-center">
+                    <h2>Oops! Something went wrong</h2>
+                    <h2>Page not found</h2>
+                    <h1>404</h1>
+                    <Button text="Go home" handleClick={goHome} />
+                
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default NotFound;
