1a9ff853d6d87ab8e307deb5dd029723db69fa57
Michał Warchoł <imsimilar@gmail.com>
2021-04-16 00:28:21 +0200

    :open_file_folder: Patterns page refactored

--- src/pages/Patterns/Patterns.tsx
+++ src/pages/Patterns/Patterns.tsx
@@ -1,14 +1,59 @@
-import React from 'react'
-import Topbar from "../../components/Topbar/Topbar";
+import React, { useEffect, useState } from 'react'
+import {useLocation} from "react-router-dom";
 
+import Topbar from "../../components/Topbar/Topbar";
+import Pattern from "../../components/Pattern/Pattern";
+import Spinner from "../../components/Spinner/Spinner";
+import Footer from "../../components/Footer/Footer";
+import {PatternType} from "../../reducers/types";
 
 const Patterns = () => {
+    const [patterns, setPatterns] = useState<PatternType[]|null>(null);
+    const location=useLocation();
+
+    useEffect(()=>{
+        getUserPatterns();
+    }, [])
+
+    const getUserPatterns = () => {
+        const query = new URLSearchParams(location.search);
+        const name = query.get('name');
+        fetch("/api/v1/palette/findByUser?user="+name, {
+			method: "get",
+			headers: { 'Content-Type': 'application/json' },
+		})
+        .then(response=>response.json())
+        .then(res=>{
+            setPatterns(res.palettes);
+        })
+    }
+
+    const generatePatterns = () => {
+        if(patterns===null){
+            return <div className="d-flex col-12 justify-content-center align-items-center"><Spinner /></div>
+        }else if(patterns.length===0){
+            return <div>no patterns</div>
+        }else{
+            const query = new URLSearchParams(location.search);
+            const name = query.get('name');
+            return <div>
+                <h2>Patterns created by {name}</h2>
+                <div className="d-flex flex-row justify-content-start flex-wrap">{
+                    patterns.map((elem, i)=>(<Pattern key={i} id={elem._id} palette={elem.palette} user={elem.user} likes={elem.likes}/>))
+                }</div>
+                </div>
+        }
+    }
+
     return(
-        <div className="patterns">
+        <div className="patterns d-flex flex-column">
             <Topbar />
-            <div className="container">
-                to be continued
+            <div className="content container d-flex flex-grow-1">
+                {
+                    generatePatterns()
+                }
             </div>
+            <Footer />
         </div>
     )
 }
