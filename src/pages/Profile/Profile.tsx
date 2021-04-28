3c11be884a2746a00327d814ecc5fe7e5c083937
Michał Warchoł <imsimilar@gmail.com>
2021-04-28 23:59:22 +0200

    :pencil2: Added carousels

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -2,46 +2,46 @@ import React, { useEffect, useState } from "react"
 import Topbar from "../../components/Topbar/Topbar";
 import Footer from "../../components/Footer/Footer";
 import Carousel from "../../components/Carousel/Carousel";
-import {PatternType} from "../../reducers/types";
+import { PatternType } from "../../reducers/types";
 
-const Profile:React.FC = () => {
+const Profile: React.FC = () => {
 
-    const [createdPatterns, setCreatedPatterns] = useState<PatternType[]|null>(null)
-    const [likedPatters, setLikedPatterns] = useState<PatternType[]|null>(null)
+    const [createdPatterns, setCreatedPatterns] = useState<PatternType[] | null>(null)
+    const [likedPatterns, setLikedPatterns] = useState<PatternType[] | null>(null)
 
-    useEffect(()=>{
+    useEffect(() => {
         const data = localStorage.getItem("user");
-        if(!data){
+        if (!data) {
             location.assign("/login");
         }
 
         fetch("/api/v1/palette/findCreatedByUser", {
-			method: "get",
-			headers: { 'Content-Type': 'application/json' },
-		})
-        .then(response=>response.json())
-        .then(res=>{
-            if(res.palettes)
-                setCreatedPatterns(res.palettes)
+            method: "get",
+            headers: { 'Content-Type': 'application/json' },
         })
-        .catch(err=>console.log(err))
+            .then(response => response.json())
+            .then(res => {
+                if (res.palettes)
+                    setCreatedPatterns(res.palettes.reverse())
+            })
+            .catch(err => console.log(err))
 
 
         fetch("/api/v1/palette/findLikedByUser", {
-			method: "get",
-			headers: { 'Content-Type': 'application/json' },
-		})
-        .then(response=>response.json())
-        .then(res=>{
-            setLikedPatterns(res.palettes)
+            method: "get",
+            headers: { 'Content-Type': 'application/json' },
         })
-        .catch(err=>console.log(err))
+            .then(response => response.json())
+            .then(res => {
+                setLikedPatterns(res.palettes.reverse())
+            })
+            .catch(err => console.log(err))
 
     }, [])
 
     const getUserName = () => {
         const data = localStorage.getItem("user");
-        if(data){
+        if (data) {
             const user = JSON.parse(data);
             return user.name;
         }
@@ -49,42 +49,42 @@ const Profile:React.FC = () => {
 
     const getUserLikedPatternsLength = () => {
         const data = localStorage.getItem("user");
-        if(data){
+        if (data) {
             const user = JSON.parse(data);
             return user.likedPalettes.length;
         }
     }
 
     const getUserCreatedPatternsLength = () => {
-        if(createdPatterns)
+        if (createdPatterns)
             return createdPatterns.length
         else return []
     }
 
-    const getUserCreatedPatterns = () => {
-        return createdPatterns;
-    }
-
-    const getUserLikedPatterns = () => {
-        return likedPatters;
-    }
-
-    return(
-    <div className="profile d-flex flex-column">
-        <Topbar />
-        <div className="content container d-flex flex-column flex-grow-1">
-            <div className="information">
-                <h1>{getUserName()}</h1>
-                <h4>Total created patterns: {getUserCreatedPatternsLength()}</h4>
-                <h4>Total liked patterns: {getUserLikedPatternsLength()}</h4>
+    return (
+        <div className="profile d-flex flex-column">
+            <Topbar />
+            <div className="content container d-flex flex-column flex-grow-1">
+                <div className="information">
+                    <h1>{getUserName()}</h1>
+                    <h4>Total patterns created: {getUserCreatedPatternsLength()}</h4>
+                    <h4>Total patterns liked: {getUserLikedPatternsLength()}</h4>
+                </div>
+                <div className="carouselBox">
+                    <h2>Created patterns</h2>
+                    {createdPatterns && <div className="d-flex flex-row">
+                        <Carousel patterns={createdPatterns} />
+                    </div>}
+                </div>
+                <div className="carouselBox">
+                    <h2>Liked Pattterns</h2>
+                    {likedPatterns && <div>
+                        <Carousel patterns={likedPatterns} />
+                    </div>}
+                </div>
             </div>
-            {createdPatterns && <div>
-                <Carousel patterns={createdPatterns}/>
-            </div>}
-            
+            <Footer />
         </div>
-        <Footer />
-    </div>
     )
 }
 
