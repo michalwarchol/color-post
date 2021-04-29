044369f3ea33fb092b6b7f32b3ce094c38594514
Michał Warchoł <imsimilar@gmail.com>
2021-04-29 02:24:39 +0200

    :hammer: Deleted unused imports

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const result = require("dotenv").config();
 
