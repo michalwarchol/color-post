e69c9c1276fe8f015b12ccfdc019d844a9ac9c9e
Michał Warchoł <imsimilar@gmail.com>
2021-05-09 01:38:06 +0200

    :smiley: Explore page created

---
+++ src/pages/Explore/Explore.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Topbar from "../../components/Topbar/Topbar";
+import Footer from "../../components/Footer/Footer";
+import BigCarousel from "../../components/BigCarousel/BigCarousel";
+
+const Explore:React.FC = () => {
+    return(
+        <div className="explore">
+            <Topbar />
+            <div className="content container d-flex flex-column">
+                <h1>Explore</h1>
+                <BigCarousel />
+            </div>
+            <Footer />
+        </div>
+    )
+}
+export default Explore;
