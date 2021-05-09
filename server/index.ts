366f9d8fc4cd742840d59224ea99eae4bba3dcb3
Michał Warchoł <imsimilar@gmail.com>
2021-05-09 03:44:43 +0200

    :gear: js -> ts

---
+++ server/index.ts
@@ -0,0 +1,43 @@
+import "dotenv-safe/config";
+import express, {Application, Request, Response} from "express";
+import path from "path";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/authRoutes";
+import paletteRoutes from "./routes/paletteRoutes";
+
+const app: Application = express();
+const port = process.env.PORT || 3000;
+const DIST_DIR = path.join(__dirname, '../dist');
+const HTML_FILE = path.join(DIST_DIR, 'index.html');
+
+//connection to MongoDB Atlas
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri as string,
+ {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}).catch((err: Error) =>console.log('Error message: '+err)
+ );
+
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+    console.log("MongoDB database connection granted!");
+})
+
+//middleware
+app.use(express.json());
+app.use(express.static(DIST_DIR));
+app.use(cookieParser());
+
+//routes
+app.use("/", authRoutes);
+app.use("/api/v1/palette", paletteRoutes);
+
+//responds index.html from dist folder
+app.get('/*', (_: Request, res: Response) => {
+    res.sendFile(HTML_FILE);
+})
+
+
+app.listen(port, function () {
+ console.log('App listening on port: ' + port);
+});
