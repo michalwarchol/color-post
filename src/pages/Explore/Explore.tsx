5c5a40334df848a3fb16a1fb42998480408d00fe
Michał Warchoł <imsimilar@gmail.com>
2021-06-05 20:20:38 +0200

    :pick: Styled /explore page

--- src/pages/Explore/Explore.tsx
+++ src/pages/Explore/Explore.tsx
@@ -7,8 +7,10 @@ const Explore:React.FC = () => {
     return(
         <div className="explore d-flex flex-column">
             <Topbar />
-            <div className="content container d-flex flex-column">
+            <div className="content container d-flex flex-column flex-grow-1 justify-content-center">
                 <h1>Explore</h1>
+                <BigCarousel />
+                <BigCarousel />
                 <BigCarousel />
             </div>
             <Footer />
