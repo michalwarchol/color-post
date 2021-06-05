87a8747ad8b707168923da9ea645a61307f97c71
Michał Warchoł <imsimilar@gmail.com>
2021-06-05 20:34:20 +0200

    :information_source: Added more info to Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,9 +5,9 @@ const Footer:React.FC = () => {
     return (
         <div className="footer">
             <div className="innerFooter container">
+                <p>You can find code of this project at <a href="https://github.com/michalwarchol/color-post">Github</a></p>
                 <p>Copyright &copy; {new Date().getFullYear()} Michał Warchoł. All rights reserved</p>
             </div>
-            
         </div>
     )
 }
