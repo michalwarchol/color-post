ffb7a435b1e8c75a3957bbbd455e2ee4c6a0df4e
Michał Warchoł <imsimilar@gmail.com>
2021-06-11 16:14:28 +0200

    :hammer: Errors handled

--- server/controllers/paletteController.ts
+++ server/controllers/paletteController.ts
@@ -1,191 +1,256 @@
-import {Request, Response} from "express";
-import {Palette} from "../models/palette";
-import {User} from "../models/user";
+import { Request, Response } from "express";
+import { Palette } from "../models/palette";
+import { User } from "../models/user";
 import { verify } from "jsonwebtoken";
 import { IUser, IPalette } from "../models/model";
 import { CallbackError } from "mongoose";
 
-
 const errorHandler = (err: Error) => {
-    return err.message;
-}
+  return err.message;
+};
 
 export const create = async (req: Request, res: Response) => {
-    const token: string = req.cookies.jwt;
-    const { palette } = req.body;
-    if(token){
-        try {
-            const decodedToken: any = verify(token, process.env.JWT_SECRET as string);
-            User.findOne({ _id: decodedToken.id }, (_: Error, result: IUser) => {
-                try {
-                    const newPalette = Palette.create({
-                        user: result.name,
-                        palette: palette
-                    })
-                    res.status(200).json({ palette: newPalette });
-                }
-                catch (err) {
-                    const error = errorHandler(err);
-                    res.status(400).json(error);
-                }
-            })
-        } catch (err) {
-            const error = errorHandler(err);
-            res.status(400).json(error);
-        }
-    }
-    
-}
+  const token: string = req.cookies.jwt;
+  const { palette } = req.body;
+  try {
+    if (token) {
+      const decodedToken: any = verify(token, process.env.JWT_SECRET as string);
+      User.findOne({ _id: decodedToken.id }, (err: CallbackError, result: IUser) => {
+          if(err){
+              throw new Error("Failed to find a record");
+          }
+          Palette.create({
+            user: result.name,
+            palette: palette,
+          }, (err: CallbackError, doc: IPalette)=>{
+              if(err){
+                throw new Error("Failed to find a record");
+              }
+              res.status(200).json({ palette: doc });
+          });
+          
+      });
+    } else throw new Error("JWT token expired");
+  } catch (err) {
+    const error = errorHandler(err);
+    res.status(200).json({error});
+  }
+};
 
 export const findById = async (req: Request, res: Response) => {
-    let id = req.body.id;
-    try {
-        const palette = await Palette.findById(id).exec();
-        res.status(200).json(palette);
-    } catch (err) {
-        const error = errorHandler(err);
-        res.status(400).json(error);
-    }
-}
+  let id = req.body.id;
+  try {
+    Palette.findById(id, (err: CallbackError, result: IPalette)=>{
+        if(err){
+            throw new Error("Failed to find a record");
+        }
+        res.status(200).json(result);
+    });
+  } catch (err) {
+    const error = errorHandler(err);
+    res.status(200).json({error});
+  }
+};
 
 export const findByUser = async (req: Request, res: Response) => {
-    const user: string|undefined = req.query.user?.toString();
-    if(user)
-    Palette.find({ user }, (err: Error, palettes: IPalette[]) => {
-        if (err) {
-            res.status(404).json({ redirect: false, message: "not found" })
-        }
-        res.status(200).json({ redirect: false, palettes });
-    })
-
-}
+  const user: string | undefined = req.query.user?.toString();
+  try{
+    if (user){
+        Palette.find({ user }, (err: CallbackError, palettes: IPalette[]) => {
+            if (err) {
+              throw new Error("Failed to find a record");
+            }
+            res.status(200).json({ redirect: false, palettes });
+          });
+      }else throw new Error("Parameter not provided");
+  }catch (err) {
+    const error = errorHandler(err);
+    res.status(200).json({error});
+  }
+};
 
 export const findCreatedByUser = async (req: Request, res: Response) => {
-    const token: string = req.cookies.jwt;
+  const token: string = req.cookies.jwt;
+  try{
     if (token) {
-        try {
-            const decodedToken: any = verify(token, process.env.JWT_SECRET as string);
-            await User.findOne({ _id: decodedToken.id }, (err: Error, user: IUser) => {
-                if(err){
-                    res.status(400).json({ message: "Somathing went wrong!" });
+        const decodedToken: any = verify(token, process.env.JWT_SECRET as string);
+        User.findOne(
+          { _id: decodedToken.id },
+          (err: CallbackError, user: IUser) => {
+            if (err) {
+                throw new Error("Failed to find a record");
+            }
+            Palette.find(
+              { user: user.name },
+              (err: CallbackError, palettes: IPalette[]) => {
+                if (err) {
+                    throw new Error("Failed to find a record");
                 }
-                Palette.find({ user: user.name }, (err: Error, palettes: IPalette[]) => {
-                    if (err) {
-                        res.status(400).json({ message: "Somathing went wrong!" });
-                    }
-                    res.status(200).json({ palettes });
-                })
-            })
-
-        } catch (err) {
-            res.status(500).json({ message: "Something went wrong!" });
-        }
-    }
-}
+                res.status(200).json({ palettes });
+              }
+            );
+          }
+        );
+    }else throw new Error("JWT token expired");
+  }catch(err){
+    const error = errorHandler(err);
+    res.status(200).json({error});
+  }
+  
+};
 
 export const findLikedByUser = async (req: Request, res: Response) => {
-    const token: string = req.cookies.jwt;
-    if(token){
-        try {
-            const decodedToken: any = verify(token, process.env.JWT_SECRET as string);
-            await User.findOne({ _id: decodedToken.id }, (err: Error, result: IUser) => {
+  const token: string = req.cookies.jwt;
+  try{
+    if (token) {
+        const decodedToken: any = verify(token, process.env.JWT_SECRET as string);
+        User.findOne(
+          { _id: decodedToken.id },
+          (err: Error, result: IUser) => {
+            if (err) {
+              throw new Error("Failed to find a record");
+            }
+            Palette.find(
+              { _id: { $in: result.likedPalettes } },
+              (err: CallbackError, palettes: IPalette[]) => {
                 if (err) {
-                    res.status(400).end();
+                  throw new Error("Failed to find a record");
                 }
-                try {
-                    Palette.find({ _id: { $in: result.likedPalettes} }, (err: Error, palettes: IPalette[]) => {
-                        if (err) {
-                            console.log(err)
-                        }
-                        res.status(200).json({ redirect: false, palettes })
-                    })
-                } catch (err) {
-                    console.log(err)
-                }
-            })
-        }
-        catch (err) {
-            res.status(301).json({ redirect: true, url: "/login" });
-        }
-    }else {
-        res.status(400).end();
-    }
-}
+                res.status(200).json({ palettes });
+              }
+            );
+          }
+        );
+      
+    } else throw new Error("JWT token expired");
+  }catch(err){
+    const error = errorHandler(err);
+    res.status(200).json({error});
+  }
+  
+};
 
 export const findAll = async (_: Request, res: Response) => {
-    Palette.find({}, (err: Error, palletes: IPalette[]) => {
-        if (err) {
-            const error = errorHandler(err);
-            res.status(400).json(error);
-        }
-        res.status(200).json(palletes);
-    })
-}
+  try{
+    Palette.find({}, (err: CallbackError, palletes: IPalette[]) => {
+      if (err) {
+        throw new Error("Failed to find a record");
+      }
+      res.status(200).json({palletes});
+    });
+  }catch(err){
+    const error = errorHandler(err);
+    res.status(200).json({error});
+  }
+};
 
 export const findLatest = async (req: Request, res: Response) => {
-    const more: string|undefined = req.query.more?.toString();
-    if(more){
-        let limit = parseInt(more) + 4;
-        Palette.find({}, {}, { sort: { "created_at": -1 }, limit: limit }, (err: Error, result: IPalette[]) => {
-            if (err) {
-                res.status(404).json({ message: "resource not found" });
-            }
-            res.status(200).json({ message: "found", resource: result });
-        })
-    }else{
-        res.status(404).json({ message: "resource not found" });
-    }
-}
+  const more: string | undefined = req.query.more?.toString();
+  try{
+    if (more) {
+      let limit = parseInt(more) + 4;
+      Palette.find(
+        {},
+        {},
+        { sort: { created_at: -1 }, limit: limit },
+        (err: CallbackError, result: IPalette[]) => {
+          if (err) {
+            throw new Error("Failed to find a record");
+          }
+          res.status(200).json({ message: "found", resource: result });
+        }
+      );
+    } else throw new Error("Parameter not provided");
+  }catch(err){
+    const error = errorHandler(err);
+    res.status(200).json({error});
+  }
+};
 
 export const findMostPopular = async (req: Request, res: Response) => {
-    const more: string|undefined = req.query.more?.toString();
-    if(more){
-        let limit = parseInt(more) + 4;
-        Palette.find({}, {}, { sort: { likes: -1, created_at: -1}, limit: limit }, (err: Error, result: IPalette[]) => {
-            if (err) {
-                res.status(404).json({ message: "resource not found" });
-            }
-            res.status(200).json({ message: "found", resource: result });
-        })
-    }else{
-        res.status(404).json({ message: "resource not found" });
-    }
-    
-}
+  const more: string | undefined = req.query.more?.toString();
+  try{
+    if (more) {
+      let limit = parseInt(more) + 4;
+      Palette.find(
+        {},
+        {},
+        { sort: { likes: -1, created_at: -1 }, limit: limit },
+        (err: CallbackError, result: IPalette[]) => {
+          if (err) {
+            throw new Error("Failed to find a record");
+          }
+          res.status(200).json({ message: "found", resource: result });
+        }
+      );
+    } else throw new Error("Parameter not provided");
+  }catch(err){
+    const error = errorHandler(err);
+    res.status(200).json({error});
+  }
+};
 
 export const findRandom = async (_: Request, res: Response) => {
-    Palette.countDocuments({}, (err: Error, count: number)=> {
-        if(err){
-            res.status(500).json({message: "Error 500"})
+  try{
+    Palette.countDocuments({}, (err: CallbackError, count: number) => {
+      if (err) {
+        throw new Error("Failed to find a record");
+      }
+      const random: number = Math.floor(Math.random() * count);
+      Palette.findOne(
+        {},
+        {},
+        { skip: random },
+        (err: CallbackError, result: IPalette | null) => {
+          if (err) {
+            throw new Error("Failed to find a record");
+          }
+          res.status(200).json({ palette: result });
         }
-        const random: number = Math.floor(Math.random() * count);
-        Palette.findOne({}, {}, {skip: random}, (err: CallbackError, result: IPalette|null)=>{
-            if(err){
-                res.status(500).json({message: "Error 500"})
-                return;
-            }
-            res.status(200).json({palette: result});
-        })
-    })
+      );
+    });
+  }catch(err){
+    const error = errorHandler(err);
+    res.status(200).json({error});
+  }
 }
 
 export const incrementLikes = async (req: Request, res: Response) => {
-    const { id } = req.body;
-    Palette.updateOne({ _id: id }, { $inc: { likes: 1 } }, {}, (err: Error, _: IPalette|null) => {
+  const { id } = req.body;
+  try{
+    Palette.updateOne(
+      { _id: id },
+      { $inc: { likes: 1 } },
+      {},
+      (err: CallbackError, _: IPalette | null) => {
         if (err) {
-            res.status(400).json({ message: "cant update doc" });
+          throw new Error("Failed to update a record");
         }
         res.status(200).json({ message: "Document updated" });
-    });
-
+      }
+    );
+  }catch(err){
+    const error = errorHandler(err);
+    res.status(200).json({error});
+  }
 }
 
 export const decrementLikes = async (req: Request, res: Response) => {
-    const { id } = req.body;
-    Palette.updateOne({ _id: id }, { $inc: { likes: -1 } }, {}, (err: Error, _: IPalette|null) => {
+  const { id } = req.body;
+  try{
+    Palette.updateOne(
+      { _id: id },
+      { $inc: { likes: -1 } },
+      {},
+      (err: Error, _: IPalette | null) => {
         if (err) {
-            res.status(400).json({ message: "cant update doc" });
+          throw new Error("Failed to update a record");
         }
         res.status(200).json({ message: "Document updated" });
-    })
+      }
+    );
+  }catch(err){
+    const error = errorHandler(err);
+    res.status(200).json({error});
+  }
 }
--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -3,103 +3,98 @@ import { User } from "../models/user";
 import { verify } from "jsonwebtoken";
 import { IUser } from "../models/model";
 
+const errorHandler = (err: Error) => {
+  return err.message;
+};
+
 export const findUserById = async (req: Request, res: Response) => {
   const token: string | null = req.cookies.jwt;
-  if (token) {
-    try {
+  try {
+    if (token) {
       const decodedToken: any = verify(token, process.env.JWT_SECRET as string);
       User.findOne({ _id: decodedToken.id }, (err: Error, result: IUser) => {
         if (err) {
-          res.status(404).json({ message: "Filed to find user" });
+          throw new Error("Failed to find a record");
         }
         res.status(200).json({ user: result });
       });
-    } catch (err) {
-      res.status(404).json({ message: "Failed find user" });
-    }
+    } else throw new Error("JWT token expired");
+  } catch (err) {
+    const error = errorHandler(err);
+    res.status(200).json({error});
   }
 };
 
 export const addLikedPattern = async (req: Request, res: Response) => {
-    const token: string | null = req.cookies.jwt;
-  if (token) {
-      try{
-        const decodedToken: any = verify(token, process.env.JWT_SECRET as string);
-        User.updateOne(
-            { _id: decodedToken.id },
-            { $push: { likedPalettes: req.body.pattern } },
-            {},
-            (err: Error, _: IUser) => {
-              if (err) {
-                res.status(404).json({ message: "cant update user data" });
-              }
-              res.status(200).json({ message: "Document updated" });
-            }
-          );
-      }catch(err){
-        res.status(400).json({ message: "cant add pattern to favourites" });
-      }
-  } else {
-    res.status(400).json({ message: "cant add pattern to favourites" });
+  const token: string | null = req.cookies.jwt;
+  try {
+    if (token) {
+      const decodedToken: any = verify(token, process.env.JWT_SECRET as string);
+      User.updateOne(
+        { _id: decodedToken.id },
+        { $push: { likedPalettes: req.body.pattern } },
+        {},
+        (err: Error, _: IUser) => {
+          if (err) {
+            throw new Error("Failed to update a record");
+          }
+          res.status(200).json({ message: "Document updated" });
+        }
+      );
+    } else throw new Error("JWT token expired");
+  } catch (err) {
+    const error = errorHandler(err);
+    res.status(200).json({error});
   }
 };
 
 export const removeLikedPattern = async (req: Request, res: Response) => {
-    const token: string | null = req.cookies.jwt;
-  if (token) {
-      try{
-        const decodedToken: any = verify(token, process.env.JWT_SECRET as string);
-        User.updateOne(
-            { _id: decodedToken.id },
-            { $pull: { likedPalettes: req.body.pattern } },
-            {},
-            (err: Error, _: IUser) => {
-              if (err) {
-                res.status(404).json({ message: "cant update user data" });
-              }
-              res.status(200).json({ message: "Document updated" });
-            }
-          );
-      }catch(err){
-        res
-        .status(400)
-        .json({ message: "cant remove pattern from favourites" });
-      }
-    }
-    else {
-    res.status(400).json({ message: "cant remove pattern from favourites" });
+  const token: string | null = req.cookies.jwt;
+  try {
+    if (token) {
+      const decodedToken: any = verify(token, process.env.JWT_SECRET as string);
+      User.updateOne(
+        { _id: decodedToken.id },
+        { $pull: { likedPalettes: req.body.pattern } },
+        {},
+        (err: Error, _: IUser) => {
+          if (err) {
+            throw new Error("Failed to update a record");
+          }
+          res.status(200).json({ message: "Document updated" });
+        }
+      );
+    } else throw new Error("JWT token expired");
+  } catch (err) {
+    const error = errorHandler(err);
+    res.status(200).json({error});
   }
 };
 
 export const isPatternLiked = async (req: Request, res: Response) => {
-    const token: string | null = req.cookies.jwt;
-  if (token) {
-      try{
-        const decodedToken: any = verify(token, process.env.JWT_SECRET as string);
-        User.findById(decodedToken.id, (err: Error, result: IUser) => {
-            if (err) {
-              res.status(400).json({ message: "user not found" });
-            }
-            const isInArray = result.likedPalettes.indexOf(req.body.pattern);
-            if (isInArray == -1) {
-              res
-                .status(200)
-                .json({
-                  message: "pattern not found in favourites",
-                  isFound: false,
-                });
-            } else {
-              res
-                .status(200)
-                .json({ message: "pattern found in favourites", isFound: true });
-            }
+  const token: string | null = req.cookies.jwt;
+  try {
+    if (token) {
+      const decodedToken: any = verify(token, process.env.JWT_SECRET as string);
+      User.findById(decodedToken.id, (err: Error, result: IUser) => {
+        if (err) {
+          throw new Error("Failed to find a record");
+        }
+        const isInArray = result.likedPalettes.indexOf(req.body.pattern);
+        if (isInArray == -1) {
+          res.status(200).json({
+            message: "pattern not found in favourites",
+            isFound: false,
           });
-      }catch(err){
-        res.status(400).json({ message: "cant verify user" });
-      }
-  } else {
-    res
-      .status(200)
-      .json({ message: "pattern not found in favourites", isFound: false });
+        } else {
+          res
+            .status(200)
+            .json({ message: "pattern found in favourites", isFound: true });
+        }
+      });
+    } else throw new Error("JWT token expired");
+  } catch (err) {
+    const error = errorHandler(err);
+    res.status(200).json({ error, isFound: false });
   }
 };
