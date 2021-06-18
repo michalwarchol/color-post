a76ea0dc05af6b966d5073187453d5bb66d5eee2
Michał Warchoł <imsimilar@gmail.com>
2021-06-18 16:39:20 +0200

    :art: Styled /login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-
+import InputField from "../../components/InputField/InputField";
+import Button from "../../components/Button/Button";
+import Image from "../../img/background1920x1080.jpg";
 
 const Login = () => {
 
@@ -18,43 +20,40 @@ const Login = () => {
         .then(response=>response.json())
         .then(res=>{
             if(res.errors){
-                setNameError(res.errors.name);
-                setPasswordError(res.errors.password);
+                if(res.errors.name)
+                    setNameError(res.errors.name);
+                if(res.errors.password)
+                    setPasswordError(res.errors.password);
             }else{
-                console.log(res);
                 localStorage.setItem("user", JSON.stringify(res.user))
                 location.assign("/");
             }
         })
         .catch(err=>{
-            console.log("sdsadsa")
             console.log(err);
         })
     }
 
     return (
         <div className="login_page d-flex">
-            <div className="form_login d-flex col-12 col-sm-6 col-md-3 align-items-center justify-content-center flex-column">
+            <img src={Image} alt="not found" />
+            <div className="form d-flex col-12 col-md-6 col-xl-4 align-items-center justify-content-center flex-column">
+                <h1 className="mt-auto">Color Post</h1>
                 <h2>Welcome back</h2>
-                <h3>Log in</h3>
                 <form className="d-flex flex-column" action="/login" onSubmit={loginSubmit}>
-
-                    <label>Name</label>
-                    <input type="text" name="name" onChange={e=>setName(e.target.value)}/>
-                    <span>{nameError}</span>
-
-                    <label>Password</label>
-                    <input type="password" name="password" onChange={e=>setPassword(e.target.value)}/>
-                    <span>{passwordError}</span>
-
-                    <div className="submit_button d-flex justify-content-around">
-                        <button type="submit">Login</button>
+                    <InputField name="name" label="Name" type="text" error={nameError} value={name} onChange={e=>setName(e.currentTarget.value)}/>
+                    <InputField name="password" label="Password" type="password" error={passwordError} value={password} onChange={e=>setPassword(e.currentTarget.value)}/>
+                    <div className="submit_button d-flex justify-content-around flex-column align-items-center">
+                        <Button type="submit" text="Login" handleClick={()=>{}}/>
                         <span className="d-flex align-self-center">Don't have an account?&nbsp;<a href="/signup">Sign Up</a></span>
                     </div>
                 </form>
-            </div>
-            <div className="slider">
-
+                <div className="info d-flex mt-auto justify-content-center">
+                    <div className="">
+                        <p>You can find code of this project at <a href="https://github.com/michalwarchol/color-post">Github</a></p>
+                        <p>Copyright &copy; {new Date().getFullYear()} Michał Warchoł. All rights reserved</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
