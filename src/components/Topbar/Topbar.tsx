7b75967e48e7f6f94f4595fa97fbe6ebd1c01894
Michał Warchoł <imsimilar@gmail.com>
2021-06-19 17:39:20 +0200

    :car: Password reset moved to topbar

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,61 +1,114 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
+import Button from "../Button/Button";
+import PasswordReset from "../PasswordReset/PasswordReset";
+import NotificationBadge from "../NotificationBadge/NotificationBadge";
+import { BsGear, BsGearFill } from "react-icons/bs";
 
 const Topbar = () => {
+  const [username, setUsername] = useState<string | null>(null);
+  const [showSettings, setShowSettings] = useState<boolean>(false);
+  const [passwordResetPanel, setPasswordResetPanel] = useState<boolean>(false);
+  const [passwordChanged, setPasswordChanged] = useState<boolean|null>(null);
 
-	const [username, setUsername] = useState<string | null>(null);
+  useEffect(() => {
+    fetch("/user/findById", {
+      method: "get",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        if (res.user) setUsername(res.user.name);
+        if (res.error) setUsername(null);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-	useEffect(() => {
-		fetch("/user/findById", {
-			method: "get",
-			headers: { 'Content-Type': 'application/json' },
-		}).then(response => response.json())
-			.then(res => {
-				if(res.user)
-					setUsername(res.user.name);
-				if(res.error)
-					setUsername(null);
-			})
-			.catch(err => console.log(err))
-	}, [])
-
-	const logout = () => {
-		localStorage.clear();
-		location.assign("/logout");
+  useEffect(()=>{
+	if(typeof passwordChanged === "boolean"){
+		setTimeout(()=>{
+			setPasswordChanged(null);
+		}, 3000)
 	}
+}, [passwordChanged])
 
-	return (
-		<div className="topbar">
-			<div className="upperTopbar container d-flex flex-column flex-sm-row">
-				<div className="logo col-12 col-sm-6 d-flex align-items-center justify-content-center  justify-content-sm-start">
-					<a href="/">Color Post</a>
-				</div>
-				<div className="buttons col-12 col-sm-6 d-flex flex-row justify-content-center justify-content-sm-end d-flex align-items-center">
-					{
-						username ?
-							<>
-								<div className="welcomeBox">Welcome {username}!</div>
-							</>
-							: <>
-								<a href="/login" className="loginButton">Login</a>
-								<a href="/signup" className="signupButton">Sign up</a>
-							</>
-					}
+  const handleSettings = () => {
+    setShowSettings(!showSettings);
+  };
 
-				</div>
-			</div>
-			{
-				username &&
-				<div className="lowerTopbar">
-					<div className="container d-flex flex-row justify-content-end">
-						<a href="/">Home</a>
-						<a href="/profile">Profile</a>
-						<a href="/explore">Explore</a>
-						<a onClick={logout}>Logout</a>
-					</div>
-				</div>
-			}
-		</div>
-	)
+  const passwordReset = () => {
+	setPasswordResetPanel(true);
 }
+const passwordResetCancel = () => {
+	setPasswordResetPanel(false);
+}
+
+const handlePasswordChanged = () => {
+	setPasswordChanged(!passwordChanged);
+}
+
+  const logout = () => {
+    localStorage.clear();
+    location.assign("/logout");
+  };
+
+  return (
+    <div className="topbar">
+      <div className="upperTopbar container d-flex flex-column flex-sm-row">
+        <div className="logo col-12 col-sm-6 d-flex align-items-center justify-content-center  justify-content-sm-start">
+          <a href="/">Color Post</a>
+        </div>
+        <div className="buttons col-12 col-sm-6 d-flex flex-row justify-content-center justify-content-sm-end d-flex align-items-center">
+          {username ? (
+            <>
+              <div className="welcomeBox">Welcome {username}!</div>
+              <div onClick={handleSettings}>
+                {showSettings ? <BsGearFill /> : <BsGear />}
+              </div>
+            </>
+          ) : (
+            <>
+              <a href="/login" className="loginButton">
+                Login
+              </a>
+              <a href="/signup" className="signupButton">
+                Sign up
+              </a>
+            </>
+          )}
+        </div>
+      </div>
+      {username && (
+        <>
+          <div className="lowerTopbar">
+            <div className="container d-flex flex-row justify-content-end">
+              <a href="/">Home</a>
+              <a href="/profile">Profile</a>
+              <a href="/explore">Explore</a>
+              <a onClick={logout}>Logout</a>
+            </div>
+          </div>
+          {showSettings && (
+            <div className="settings ">
+              <div className="d-flex container flex-column justify-content-end">
+                <span>Settings</span>
+                <div className="buttons d-flex flex-row justify-content-end">
+                  <Button
+                    text="Reset password"
+                    handleClick={passwordReset}
+                    type="button"
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+        </>
+      )}
+	  {passwordResetPanel && <PasswordReset onSuccess={handlePasswordChanged} cancel={passwordResetCancel} />}
+	  {(passwordChanged || !passwordChanged) && typeof passwordChanged === "boolean" && (
+                <NotificationBadge text="Password has been reset" />
+    	)}
+    </div>
+  );
+};
 
 export default Topbar;
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -2,17 +2,12 @@ import React, { useEffect, useState } from "react"
 import Topbar from "../../components/Topbar/Topbar";
 import Footer from "../../components/Footer/Footer";
 import Carousel from "../../components/Carousel/Carousel";
-import Button from "../../components/Button/Button";
-import PasswordReset from "../../components/PasswordReset/PasswordReset";
-import NotificationBadge from "../../components/NotificationBadge/NotificationBadge";
 import { PatternType } from "../../reducers/types";
 
 const Profile: React.FC = () => {
 
     const [createdPatterns, setCreatedPatterns] = useState<PatternType[] | null>(null);
     const [likedPatterns, setLikedPatterns] = useState<PatternType[] | null>(null);
-    const [passwordResetPanel, setPasswordResetPanel] = useState<boolean>(false);
-    const [passwordChanged, setPasswordChanged] = useState<boolean|null>(null);
 
     useEffect(() => {
         const data = localStorage.getItem("user");
@@ -44,14 +39,6 @@ const Profile: React.FC = () => {
 
     }, [])
 
-    useEffect(()=>{
-        if(typeof passwordChanged === "boolean"){
-            setTimeout(()=>{
-                setPasswordChanged(null);
-            }, 3000)
-        }
-    }, [passwordChanged])
-
     const getUserName = () => {
         const data = localStorage.getItem("user");
         if (data) {
@@ -74,26 +61,18 @@ const Profile: React.FC = () => {
         else return []
     }
 
-    const passwordReset = () => {
-        setPasswordResetPanel(true);
-    }
-    const passwordResetCancel = () => {
-        setPasswordResetPanel(false);
-    }
-
-    const handlePasswordChanged = () => {
-        setPasswordChanged(!passwordChanged);
-    }
-
     return (
         <div className="profile d-flex flex-column">
             <Topbar />
             <div className="content container d-flex flex-column flex-grow-1">
                 <div className="information">
-                    <h1>{getUserName()}</h1>
-                    <h4>Total patterns created: {getUserCreatedPatternsLength()}</h4>
-                    <h4>Total patterns liked: {getUserLikedPatternsLength()}</h4>
-                    <Button text="reset password" handleClick={passwordReset}/>
+                    <div className="d-flex justify-content-center">
+                        <h4>Name: {getUserName()}</h4>
+                    </div>
+                    <div className="d-flex flex-row justify-content-around">
+                        <h4>Total patterns created: {getUserCreatedPatternsLength()}</h4>
+                        <h4>Total patterns liked: {getUserLikedPatternsLength()}</h4>
+                    </div>
                 </div>
                 <div className="carouselBox">
                     {createdPatterns && <div>
@@ -106,11 +85,7 @@ const Profile: React.FC = () => {
                     </div>}
                 </div>
             </div>
-            {passwordResetPanel && <PasswordReset onSuccess={handlePasswordChanged} cancel={passwordResetCancel} />}
             <Footer />
-            {(passwordChanged || !passwordChanged) && typeof passwordChanged === "boolean" && (
-                <NotificationBadge text="Password has been reset" />
-            )}
         </div>
     )
 }
